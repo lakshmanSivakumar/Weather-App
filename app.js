0ad14f8848f204b9eb0774433e12e74590553119
Lakshman Sivakumar <lakshmansivakumar1298@gmail.com>
2021-01-19 18:59:06 +0530

    completed

---
+++ app.js
@@ -0,0 +1,46 @@
+const form = document.querySelector('form');
+const dayOrNightImg = document.querySelector('.dayOrNight');
+const details = document.querySelector('.details');
+const icon = document.querySelector('.icon');
+
+form.addEventListener('submit', city);
+
+function updateUI(data) {
+    const {cityDetails, weather} = data;
+
+    details.innerHTML = `<h3>${cityDetails.EnglishName}</h3>
+                         <h3>${weather.WeatherText}</h3>
+                         <h3>${weather.Temperature.Metric.Value} &deg; C</h3>`;
+
+    
+    const iconSrc = `./images/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+    
+    let dayOrNight = null;
+    if(weather.IsDayTime) {
+        dayOrNight = './images/day.svg';
+    } else {
+        dayOrNight = './images/night.svg';
+    }
+
+    dayOrNightImg.setAttribute('src', dayOrNight);
+        
+}
+
+const updateCity = async (cityValue) => {
+
+    const cityDetails = await getCity(cityValue);
+    const weather = await getWeather(cityDetails.Key);
+
+    return {cityDetails, weather};
+}
+
+function city(e) {
+    e.preventDefault();
+    let cityValue = form.city.value.trim();
+    form.city.value = '';
+
+    updateCity(cityValue)
+        .then( data => updateUI(data))
+        .catch( err => console.log(err));
+}
---
+++ forecast.js
@@ -0,0 +1,26 @@
+const key = 'tsEq3rrODVpXWzGgUGK8260dXOx2NGof';
+
+const getWeather = async (id) => {
+    
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+}
+
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+
+    return data[0];
+
+}
